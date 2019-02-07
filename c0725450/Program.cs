c48dd5e618d67978ebf057ceb075ed32cd87cc06
c0725450 <komalgillc3@gmail.com>
2019-02-07 14:12:45 -0500

    Commit

--- c0725450/Program.cs
+++ c0725450/Program.cs
@@ -10,6 +10,36 @@ namespace c0725450
     {
         static void Main(string[] args)
         {
+            var a = new TestQuestion2();
+            a.PlayingWithForLoops();
+        }
+    }
+
+    class TestQuestion2
+    {
+        public int myFavouriteVariable = 0;
+        public void PlayingWithForLoops()
+        {
+            // write a for loop to add 10 numbers
+            //Need to know the four types of variables in OO programming:
+            //Scoping: Visibility: What we can see??
+            // 
+           
+            for (; MyMethod(); )
+            {
+                if (myFavouriteVariable > 10)
+                {
+                    Console.WriteLine("I am so out of here!");
+                    break;
+                }
+                Console.WriteLine("Oh no I have to go through this stupid loop again...");
+            }
+
+        }
+        public bool MyMethod()
+        {
+            myFavouriteVariable++;
+            return true;
         }
     }
 }
